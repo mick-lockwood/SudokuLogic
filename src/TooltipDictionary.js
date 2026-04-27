6a3df39ae79fe086b87a7ae430a0eba5be9b4152
mick-lockwood <mick.lockwood87@gmail.com>
2026-04-27 15:58:21 +0930

    Update TooltipDictionary.js

--- src/TooltipDictionary.js
+++ src/TooltipDictionary.js
@@ -3,28 +3,29 @@
 export const Tooltips = {
     // Core Tools
     'tool-pointer':
-            `Select cells and input numbers.\n
-    Shortcut: Esc or V`,
+`Select cells and input numbers.\n
+Shortcut: Esc or V`,
     
     'tool-eraser':
-            `Click any drawn variant graphic to delete it.`,
+`Click any drawn variant graphic to delete it.`,
     
     // Variant Tools
     'tool-thermo':
-            `Numbers must strictly increase
-    from the bulb to the tip.\n
-    Click and drag on cells to draw.\n
-    Line starts with the bulb when drawing.`,
+`Numbers must strictly increase
+from the bulb to the tip.\n
+Click and drag on cells to draw.\n
+Line starts with the bulb when drawing.`,
     
     'tool-whisper':
-            `Adjacent numbers along the line must
-    have a difference of 5 or more.\n
-    Click and drag on cells to draw.`,
+`Adjacent numbers along the line must
+have a difference of 5 or more.\n
+Click and drag on cells to draw.`,
     
     // Actions (Optional, but good to centralize!)
     'btn-undo-line':
-            `Undo the last drawn variant line.`,
+`Undo the last drawn variant line.`,
     
     'btn-clear-variants':
-            `Instantly clear all variant lines from the board.`
+`Instantly clear all variant lines from the board.`
+    
 };
